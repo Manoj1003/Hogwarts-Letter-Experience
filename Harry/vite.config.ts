3d0dee20414c0755dfe92576a442da2cfdb22973
Manoj1003 <provision2k24@gmail.com>
2026-08-20 05:16:38 +0530

    Fix Vite multi-page deployment

--- Harry/vite.config.ts
+++ Harry/vite.config.ts
@@ -1,18 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, '..');
 
 export default defineConfig({
   plugins: [react()],
 
+  base: './',
+
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',
-        letter: 'letter.html',
-        sorting: 'sorting.html',
-        wand: 'wand.html',
-        Patronus: 'Patronus.html',
-        boggarts: 'boggarts.html',
+        index: resolve(__dirname, 'index.html'),
+        Army: resolve(__dirname, 'Army.html'),
+        boggarts: resolve(__dirname, 'boggarts.html'),
+        book: resolve(__dirname, 'book.html'),
+        creature: resolve(__dirname, 'creature.html'),
+        curses: resolve(__dirname, 'curses.html'),
+        deadly: resolve(__dirname, 'deadly.html'),
+        duels: resolve(__dirname, 'duels.html'),
+        letter: resolve(__dirname, 'letter.html'),
+        movie: resolve(__dirname, 'movie.html'),
+        news: resolve(__dirname, 'news.html'),
+        Patronus: resolve(__dirname, 'Patronus.html'),
+        places: resolve(__dirname, 'places.html'),
+        sorting: resolve(__dirname, 'sorting.html'),
+        spells: resolve(__dirname, 'spells.html'),
+        wand: resolve(__dirname, 'wand.html'),
       },
     },
   },
